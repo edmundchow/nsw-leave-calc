4079740d666e02f84cf1ae2473165d08b9ada33e
edmundchow <edmundchow00@gmail.com>
2026-05-07 04:49:16 +1000

    Update service worker to version 5 and simplify caching

--- sw.js
+++ sw.js
@@ -1,43 +1,14 @@
-const CACHE_NAME = 'nsw-leave-v4';
-const ASSETS = [
-  './',
-  './index.html',
-  './app.js',
-  './manifest.json'
-];
+const CACHE_NAME = 'nsw-leave-v5';
+const ASSETS = ['./', './index.html', './app.js', './manifest.json'];
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      // Use addAll but catch individual failures to prevent complete install failure
-      return Promise.allSettled(
-        ASSETS.map(url => cache.add(url).catch(err => console.warn(`Failed to cache ${url}:`, err)))
-      );
-    })
-  );
-});
-
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)));
-    })
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
 self.addEventListener('fetch', (e) => {
   if (e.request.url.includes('data.gov.au')) {
-    e.respondWith(
-      caches.open('api-cache').then(c => 
-        fetch(e.request).then(r => {
-          c.put(e.request, r.clone());
-          return r;
-        }).catch(() => c.match(e.request))
-      )
-    );
+    e.respondWith(caches.open('api-cache').then(c => fetch(e.request).then(r => { c.put(e.request, r.clone()); return r; }).catch(() => c.match(e.request))));
   } else {
-    e.respondWith(
-      caches.match(e.request).then(res => res || fetch(e.request))
-    );
+    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
   }
 });
