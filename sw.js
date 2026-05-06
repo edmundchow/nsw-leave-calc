9fee19e0e32922cf90036f043844b8d2fc3d574b
edmundchow <edmundchow00@gmail.com>
2026-05-06 20:12:51 +1000

    Update service worker for caching and assets

--- sw.js
+++ sw.js
@@ -1,22 +1,9 @@
-const CACHE_NAME = 'nsw-leave-calc-v1';
-const assets = [
-  './',
-  './index.html',
-  './manifest.json'
-];
+const CACHE_NAME = 'nsw-leave-v3';
+const ASSETS = ['./', './index.html', './app.js', './manifest.json'];
 
-self.addEventListener('install', evt => {
-  evt.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      cache.addAll(assets);
-    })
-  );
-});
-
-self.addEventListener('fetch', evt => {
-  evt.respondWith(
-    caches.match(evt.request).then(res => {
-      return res || fetch(evt.request);
-    })
-  );
+self.addEventListener('install', (e) => { e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))); });
+self.addEventListener('fetch', (e) => {
+    if (e.request.url.includes('data.gov.au')) {
+        e.respondWith(caches.open('api-cache').then(c => fetch(e.request).then(r => { c.put(e.request, r.clone()); return r; }).catch(() => c.match(e.request))));
+    } else { e.respondWith(caches.match(e.request).then(res => res || fetch(e.request))); }
 });
