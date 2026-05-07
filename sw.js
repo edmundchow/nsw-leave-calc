d072a64a5351f9601e6410bd9281dea79aa4b84e
edmundchow <edmundchow00@gmail.com>
2026-05-07 19:44:38 +1000

    Update service worker cache name and fetch logic

--- sw.js
+++ sw.js
@@ -1,16 +1,29 @@
-const CACHE_NAME = 'nsw-leave-v10'; 
+const CACHE_NAME = 'nsw-leave-v12'; 
 const ASSETS = ['./', './index.html', './app.js', './manifest.json'];
+
 self.addEventListener('install', (e) => {
   self.skipWaiting();
   e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
+
 self.addEventListener('activate', (e) => {
-  e.waitUntil(caches.keys().then((keys) => Promise.all(keys.map((key) => { if (key !== CACHE_NAME) return caches.delete(key); }))));
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(keys.map((key) => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      }));
+    })
+  );
 });
+
 self.addEventListener('fetch', (e) => {
-  e.respondWith(fetch(e.request).then((res) => {
-    const resClone = res.clone();
-    caches.open(CACHE_NAME).then((cache) => { cache.put(e.request, resClone); });
-    return res;
-  }).catch(() => caches.match(e.request)));
+  e.respondWith(
+    fetch(e.request)
+      .then((res) => {
+        const resClone = res.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(e.request, resClone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
 });
