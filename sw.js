7404914b2de747f1ddfa0f8a4402d906e38b18cb
edmundchow <edmundchow00@gmail.com>
2026-05-07 19:56:09 +1000

    Update CACHE_NAME from v12 to v13

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'nsw-leave-v12'; 
+const CACHE_NAME = 'nsw-leave-v13'; 
 const ASSETS = ['./', './index.html', './app.js', './manifest.json'];
 
 self.addEventListener('install', (e) => {
